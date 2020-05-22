5c72798fdaa9eabb86a0273ebb4db1fc9fa38f2b
ofer baram <my.phone.il@gmail.com>
2020-05-22 17:42:24 +0300

    gh- package.json updated

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <div class="main-container">
     <div class="main-title-container">
       <h1 class="title">Three Card Monte</h1>
-      <!-- <h2 class="subtitle">Can you bit the bank?</h2> -->
     </div>
     <Game />
   </div>
